53cc4a48f91b34991956a7223e70ed5a80920af7
sondrea <sandviksandra424@gmail.com>
2025-01-19 19:43:43 +0100

    setter in S i melingen

---
+++ src/pages/app.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import NavBar from "./randomshit/navbar"
+
+function App() {
+    return (
+        <>
+            <h1>Jeg elsker ikke zesty men</h1>
+            <NavBar/>
+            <p>COmmMIOTIJPOTIJTPOIJGPOEIWJ</p>
+        </>
+    )
+}
+export default App
---
+++ src/pages/randomshit/navbar.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+function NavBar() {
+    return(
+        <>
+        <h1>mmSmmmmmmmmmmmmmmmmmmmmmmm is</h1>
+        <p>eg elske kakker</p>
+        </>
+    )
+}
+
+export default NavBar
+
